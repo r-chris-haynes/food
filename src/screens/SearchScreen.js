2d7482a509459ec578bae67ca6863978e73fd29e
chris haynes <r.chris.haynes@gmail.com>
2021-01-12 18:11:01 -0500

    added error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,39 +1,37 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet} from 'react-native';
-import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp'
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'durham'
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "durham",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong')
+    }
+  };
 
-      }
-    });
-    setResults(response.data.businesses)
-  }
-
-  return(
+  return (
     <View>
-      <SearchBar 
-      term={term} 
-      onTermChange={setTerm}
-      onTermSubmit={searchApi}
-      />
-      <Text>Search Screen</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default SearchScreen;
