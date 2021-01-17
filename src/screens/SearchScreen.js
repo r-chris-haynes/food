dd4632dea35d47282a0a7df1ea442ab4a8c88e00
chris haynes <r.chris.haynes@gmail.com>
2021-01-17 16:03:03 -0500

    got title and set of data to show inside of resultsList

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,14 +8,20 @@ const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
   
+  const filterResultsByPrice = (price) => {
+    return results.filter(result => {
+      return result.price === price;
+    });
+  };
+
   return (
     <View>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
-      <ResultsList title='Cost Effective' />
-      <ResultsList title='Bit Pricier' />
-      <ResultsList title='Big Spender' />
+      <ResultsList results={filterResultsByPrice('$')} title='Cost Effective'  />
+      <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier' />
+      <ResultsList results={filterResultsByPrice('$$$')}  title='Big Spender' />
     </View>
   );
 };
