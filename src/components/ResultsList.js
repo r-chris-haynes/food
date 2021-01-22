a2ad19c268c27fe7762d730e325130ec2df86c84
chris haynes <r.chris.haynes@gmail.com>
2021-01-22 12:51:47 -0500

    added styling to ResultsDetail and ResultsList components

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 import ResultsDetail from '../components/ResultsDetail'
 
 const ResultsList = ({ title, results }) => {
-  return <View>
+  return <View style={styles.container}>
     <Text style={styles.title}>{title}</Text>
     <FlatList 
       horizontal
@@ -18,8 +18,13 @@ const ResultsList = ({ title, results }) => {
 
 const styles = StyleSheet.create({
   title: {
+    marginBottom: 5,
+    marginLeft: 15,
     fontSize: 18,
     fontWeight: 'bold'
+  },
+  container: {
+    marginBottom: 10
   }
 });
 
