19232327b6b5b47f438c1086763cd77e8328d672
chris haynes <r.chris.haynes@gmail.com>
2021-01-22 12:57:52 -0500

    removed scroll bar and added scrollView element

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import SearchBar from "../components/SearchBar";
-import useResults from '../hooks/useResults';
-import ResultsList from '../components/ResultsList';
+import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
-  
+
   const filterResultsByPrice = (price) => {
-    return results.filter(result => {
+    return results.filter((result) => {
       return result.price === price;
     });
   };
@@ -19,9 +19,17 @@ const SearchScreen = () => {
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
-      <ResultsList results={filterResultsByPrice('$')} title='Cost Effective'  />
-      <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier' />
-      <ResultsList results={filterResultsByPrice('$$$')}  title='Big Spender' />
+      <ScrollView>
+        <ResultsList
+          results={filterResultsByPrice("$")}
+          title="Cost Effective"
+        />
+        <ResultsList results={filterResultsByPrice("$$")} title="Bit Pricier" />
+        <ResultsList
+          results={filterResultsByPrice("$$$")}
+          title="Big Spender"
+        />
+      </ScrollView>
     </View>
   );
 };
